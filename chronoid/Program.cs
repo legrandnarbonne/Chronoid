3ebef3f262a17745c132d45a9c8584c69bf4a187
DAPOJERO <DAPOJERO@DAPOJERO>
2020-01-06 12:05:51 +0100

    Ajoutez des fichiers projet.

---
+++ chronoid/Program.cs
@@ -0,0 +1,118 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: DAPOJERO
+ * Date: 27/01/2013
+ * Time: 18:22
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Net;
+using System.Xml;
+using System.Reflection;
+using System.IO;
+
+namespace chronoid
+{
+	class Program
+	{
+        static String _appPath;
+		public static void Main(string[] args)
+		{
+            _appPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+            if (config.charge())
+			{
+				for(int a=0;a<config.URL.Length;a++)
+				{
+					log(getServiceResult(config.URL[a]),config.LogsPath[a]);
+				}
+			}
+			else
+				log(config.LastErreur, _appPath);
+			
+		}
+		private static void log(string msg,string chemin)
+		{
+            var dir = new DirectoryInfo(chemin);
+
+            dir.Create();
+
+			chemin+="\\chronoid.log";
+			Console.Write(msg);
+			
+			StreamWriter sw=new StreamWriter(chemin,true);
+			sw.WriteLine(DateTime.Now+" "+msg);
+			sw.Close();
+			
+		}
+
+		public static string getServiceResult(string serviceUrl) {
+
+            log("Requesting :" + serviceUrl, _appPath);
+
+            HttpWebRequest HttpWReq;
+			HttpWebResponse HttpWResp;
+			HttpWReq = (HttpWebRequest)WebRequest.Create(serviceUrl);
+			
+			CredentialCache cc = new CredentialCache();
+
+
+            HttpWReq.Credentials = CredentialCache.DefaultCredentials;
+
+            if (!string.IsNullOrEmpty(config.User))
+            {
+                HttpWReq.Credentials = BuildCredentials(serviceUrl, config.User, config.Password, "NTLM");
+                log(config.LastErreur, _appPath);
+            }
+
+            HttpWReq.Method = "Post";
+			HttpWReq.ContentLength=0;
+
+            
+
+            HttpWResp = (HttpWebResponse)HttpWReq.GetResponse();
+			
+			if (HttpWResp.StatusCode == HttpStatusCode.OK)
+			{
+				//Consume webservice with basic XML reading, assumes it returns (one) string
+				XmlReader reader = XmlReader.Create(HttpWResp.GetResponseStream());
+				while (reader.Read())
+				{
+					reader.MoveToFirstAttribute();
+					if (reader.NodeType == XmlNodeType.Text)
+					{
+						return reader.Value;
+					}
+				}
+				return string.Empty;
+			}
+			else
+			{
+				throw new Exception("Error on remote : "+ HttpWResp.StatusCode.ToString());
+			}
+		}
+
+        private static ICredentials BuildCredentials(string siteurl, string username, string password, string authtype)
+        {
+            NetworkCredential cred;
+            if (username.Contains(@"\"))
+            {
+                string domain = username.Substring(0, username.IndexOf(@"\", StringComparison.InvariantCulture));
+                username = username.Substring(username.IndexOf(@"\", StringComparison.InvariantCulture) + 1);
+                cred = new NetworkCredential(username, password, domain);
+            }
+            else
+            {
+                cred = new NetworkCredential(username, password);
+            }
+            CredentialCache cache = new CredentialCache();
+            if (authtype.Contains(":"))
+            {
+                authtype = authtype.Substring(authtype.IndexOf(":",StringComparison.InvariantCulture) + 1); //remove the TMG: prefix
+            }
+            cache.Add(new Uri(siteurl), authtype, cred);
+            return cache;
+        }
+    }
+}
---
+++ chronoid/config.cs
@@ -0,0 +1,84 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: DAPOJERO
+ * Date: 19/03/2009
+ * Time: 09:49
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+
+using System;
+using System.Text;
+using System.IO;
+
+namespace chronoid
+{
+	/// <summary>
+	/// Description of config.
+	/// </summary>
+	public static class config
+	{
+		public static string[] URL { get; set; }
+		public static string[] LogsPath { get; set; }
+        public static string User { get; set; }
+        public static string Password { get; set; }
+        public static string Domain { get; set; }
+
+        public static string LastErreur { get; set; }
+
+        public static bool charge()
+		{
+			
+			FileInfo fi = new FileInfo("chronoid.ini");
+			
+			if (fi.Exists) 
+			{
+				StreamReader sr = new StreamReader("chronoid.ini", Encoding.Default);
+				string s = sr.ReadToEnd();
+				sr.Close();
+				string[] conf=s.Split('\n','=');
+				int cmpt=0;
+				
+				
+				while (cmpt<(conf.Length-1))
+				{
+					string val;
+					try
+					{
+						val=conf[cmpt+1].Trim();
+						
+						switch (conf[cmpt].ToLower())
+						{
+							case "urls":
+								URL=val.Split(';');
+								break;
+							case "logs":
+								LogsPath=val.Split(';');
+								break;
+							case "domaine":
+								Domain=val;
+								break;
+							case "utilisateur":
+								User=val;
+								break;
+							case "motdepasse":
+								Password=val;
+								break;
+						}
+					}
+					catch(Exception e){
+						LastErreur=e.Message;
+						return false;
+					}
+					cmpt++;
+				}
+				return true;
+			}
+			else
+			{
+				LastErreur="Fichier chronoid.ini introuvable.";
+				return false;
+			}
+		}
+	}
+}
