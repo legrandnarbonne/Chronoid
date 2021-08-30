5edec17c6e24788423a653771f2da190fc092d44
legrandnarbonne <legrandnarbonne@legrandnarbonne>
2021-08-30 08:54:31 +0200

    .

--- chronoid/Program.cs
+++ chronoid/Program.cs
@@ -21,11 +21,13 @@ namespace chronoid
 		{
             _appPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
 
-            if (config.charge())
+            var configFile = args.Length > 0 ? args[0] : null;
+
+            if (config.charge(configFile))
 			{
 				for(int a=0;a<config.URL.Length;a++)
 				{
-					log(getServiceResult(config.URL[a]),config.LogsPath[a]);
+					log(getServiceResult(config.URL[a],config.Method[a]),config.LogsPath[a]);
 				}
 			}
 			else
@@ -47,50 +49,58 @@ namespace chronoid
 			
 		}
 
-		public static string getServiceResult(string serviceUrl) {
+		public static string getServiceResult(string serviceUrl,string method) {
 
             log("Requesting :" + serviceUrl, _appPath);
-
-            HttpWebRequest HttpWReq;
-			HttpWebResponse HttpWResp;
-			HttpWReq = (HttpWebRequest)WebRequest.Create(serviceUrl);
-			
-			CredentialCache cc = new CredentialCache();
-
-
-            HttpWReq.Credentials = CredentialCache.DefaultCredentials;
-
-            if (!string.IsNullOrEmpty(config.User))
+            try
             {
-                HttpWReq.Credentials = BuildCredentials(serviceUrl, config.User, config.Password, "NTLM");
-                log(config.LastErreur, _appPath);
+                HttpWebRequest HttpWReq;
+                HttpWebResponse HttpWResp;
+                HttpWReq = (HttpWebRequest)WebRequest.Create(serviceUrl);
+
+                CredentialCache cc = new CredentialCache();
+
+
+                HttpWReq.Credentials = CredentialCache.DefaultCredentials;
+
+                if (!string.IsNullOrEmpty(config.User))
+                {
+                    HttpWReq.Credentials = BuildCredentials(serviceUrl, config.User, config.Password, "NTLM");
+                    log(config.LastErreur, _appPath);
+                }
+
+                HttpWReq.Method =string.IsNullOrEmpty(method)? "Post":method;
+                HttpWReq.ContentLength = 0;
+
+
+
+                HttpWResp = (HttpWebResponse)HttpWReq.GetResponse();
+
+                if (HttpWResp.StatusCode == HttpStatusCode.OK)
+                {
+                    //Consume webservice with basic XML reading, assumes it returns (one) string
+                    XmlReader reader = XmlReader.Create(HttpWResp.GetResponseStream());
+                    while (reader.Read())
+                    {
+                        reader.MoveToFirstAttribute();
+                        if (reader.NodeType == XmlNodeType.Text)
+                        {
+                            return reader.Value;
+                        }
+                    }
+                    return string.Empty;
+                }
+                else
+                {
+                    log("Getting response :" + HttpWResp.StatusCode.ToString(), _appPath);
+                    throw new Exception("Error on remote : " + HttpWResp.StatusCode.ToString());
+                }
             }
-
-            HttpWReq.Method = "Post";
-			HttpWReq.ContentLength=0;
-
-            
-
-            HttpWResp = (HttpWebResponse)HttpWReq.GetResponse();
-			
-			if (HttpWResp.StatusCode == HttpStatusCode.OK)
-			{
-				//Consume webservice with basic XML reading, assumes it returns (one) string
-				XmlReader reader = XmlReader.Create(HttpWResp.GetResponseStream());
-				while (reader.Read())
-				{
-					reader.MoveToFirstAttribute();
-					if (reader.NodeType == XmlNodeType.Text)
-					{
-						return reader.Value;
-					}
-				}
-				return string.Empty;
-			}
-			else
-			{
-				throw new Exception("Error on remote : "+ HttpWResp.StatusCode.ToString());
-			}
+            catch(Exception e)
+            {
+                log("Erreur :" + e.ToString(), _appPath);
+                return null;
+            }
 		}
 
         private static ICredentials BuildCredentials(string siteurl, string username, string password, string authtype)
--- chronoid/config.cs
+++ chronoid/config.cs
@@ -13,72 +13,80 @@ using System.IO;
 
 namespace chronoid
 {
-	/// <summary>
-	/// Description of config.
-	/// </summary>
-	public static class config
-	{
-		public static string[] URL { get; set; }
-		public static string[] LogsPath { get; set; }
+    /// <summary>
+    /// Description of config.
+    /// </summary>
+    public static class config
+    {
+        public static string[] URL { get; set; }
+        public static string[] Method { get; set; }
+        public static string[] LogsPath { get; set; }
         public static string User { get; set; }
         public static string Password { get; set; }
         public static string Domain { get; set; }
 
         public static string LastErreur { get; set; }
 
-        public static bool charge()
-		{
-			
-			FileInfo fi = new FileInfo("chronoid.ini");
-			
-			if (fi.Exists) 
-			{
-				StreamReader sr = new StreamReader("chronoid.ini", Encoding.Default);
-				string s = sr.ReadToEnd();
-				sr.Close();
-				string[] conf=s.Split('\n','=');
-				int cmpt=0;
-				
-				
-				while (cmpt<(conf.Length-1))
-				{
-					string val;
-					try
-					{
-						val=conf[cmpt+1].Trim();
-						
-						switch (conf[cmpt].ToLower())
-						{
-							case "urls":
-								URL=val.Split(';');
-								break;
-							case "logs":
-								LogsPath=val.Split(';');
-								break;
-							case "domaine":
-								Domain=val;
-								break;
-							case "utilisateur":
-								User=val;
-								break;
-							case "motdepasse":
-								Password=val;
-								break;
-						}
-					}
-					catch(Exception e){
-						LastErreur=e.Message;
-						return false;
-					}
-					cmpt++;
-				}
-				return true;
-			}
-			else
-			{
-				LastErreur="Fichier chronoid.ini introuvable.";
-				return false;
-			}
-		}
-	}
+        public static bool charge(string fileName)
+        {
+
+            if (string.IsNullOrEmpty(fileName)) fileName = "chronoid.ini";
+
+
+            FileInfo fi = new FileInfo(fileName);
+
+            if (fi.Exists)
+            {
+                StreamReader sr = new StreamReader(fileName, Encoding.Default);
+                string s = sr.ReadToEnd();
+                sr.Close();
+                string[] conf = s.Split('\n');
+                int cmpt = 0;
+
+
+                while (cmpt < (conf.Length - 1))
+                {
+                    string val = conf[cmpt].Trim();
+                    var part = val.Split(new char[] { '=' }, 2);
+                    if (part.Length > 1)
+                        try
+                        {
+                            switch (part[0].ToLower())
+                            {
+                                case "urls":
+                                    URL = part[1].Split(';');
+                                    break;
+                                case "metode":
+                                    Method = part[1].Split(';');
+                                    break;
+                                case "logs":
+                                    LogsPath = part[1].Split(';');
+                                    break;
+                                case "domaine":
+                                    Domain = val;
+                                    break;
+                                case "utilisateur":
+                                    User = part[1];
+                                    break;
+                                case "motdepasse":
+                                    Password = part[1];
+                                    break;
+                            }
+                        }
+                        catch (Exception e)
+                        {
+                            LastErreur = e.Message;
+                            return false;
+                        }
+                    cmpt++;
+                }
+                return true;
+            }
+            else
+            {
+                LastErreur = $"Fichier {fileName} introuvable.";
+                return false;
+            }
+        }
+    }
 }
